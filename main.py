189864fd9ba36f209121024063e766cc7ff226b8
noahdominic <noahdmnc@gmail.com>
2024-12-01 14:32:22 +0800

    Run Day 01, Part 2 in main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from aoc00 import greet
-from aoc01 import day01_01
+from aoc01 import day01_01, day01_02
 
 def main ():
     greet("Charlie")
     day01_01()
+    day01_02()
 
 if __name__ == "__main__":
     main()

17e16f482247ad5a3cb51635395feda46dfd3b0d
noahdominic <noahdmnc@gmail.com>
2024-12-05 02:40:32 +0800

    Solve day 3 part 2

--- aoc03.py
+++ aoc03.py
@@ -13,4 +13,28 @@ def day03_01(file_path):
 
         print(total)
 
+def day03_02(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        command_matches = re.finditer(r"do\(\)|don't\(\)", lines[0])
+        num_matches = re.finditer(r"mul\(([0-9]+),([0-9]+)\)", lines[0])
+
+        commands = [{"type": "command", "content": match.group(0), "position": match.span()[0]} for match in command_matches]
+        numbers = [
+            {"type": "number_pair", "content": (int(match.group(1)), int(match.group(2))), "position": match.span()[0]}
+                for match in num_matches
+        ]
+
+        merged_list = sorted(commands + numbers, key=lambda x: x["position"])
+
+        active = True
+        total = 0
+
+        for item in merged_list:
+            if item["type"] == "command":
+                active = item["content"] == "do()"
+            elif item["type"] == "number_pair" and active:
+                total += item["content"][0] * item["content"][1]
+
+        print(total)
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aoc00 import greet
 from aoc01 import day01_01, day01_02
 from aoc02 import day02_01, day02_02
-from aoc03 import day03_01
+from aoc03 import day03_01, day03_02
 
 def main ():
     greet("Charlie")
@@ -10,6 +10,7 @@ def main ():
     # day02_01("input/02.txt")
     # day02_02("input/02.txt")
     day03_01("input/03.txt")
+    day03_02("input/03.txt")
 
 if __name__ == "__main__":
     main()

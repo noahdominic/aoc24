b73e19ea2942d80604d50d7d425efa2cb91f2cf8
noahdominic <noahdmnc@gmail.com>
2024-12-10 00:35:13 +0800

    Solve day 9 part 1

--- aoc09.py
+++ aoc09.py
@@ -1,4 +1,15 @@
 # Advent of Code Day 09
+from itertools import zip_longest
+
+
+
+def pop_rightmost_number(mixed_list):
+    for i in range(len(mixed_list) - 1, -1, -1):  # Traverse list from the end
+        if isinstance(mixed_list[i], (int, float)):  # Check if the item is a number
+            return mixed_list.pop(i)  # Remove and return the number
+    return None
+
+
 
 def day09_01(input_data):
     """
@@ -6,7 +17,28 @@ def day09_01(input_data):
     :param input_data: The input data as a string.
     :return: The solution to part 1.
     """
-    pass
+    checksum = 0
+
+    data = input_data[0]
+
+    numbers = data[::2]
+    gaps = data[1::2]
+
+    decoded = []
+    reverse_decoded = []
+    for i, (number, gap) in enumerate(zip_longest(numbers, gaps, fillvalue='0')):
+        decoded += [i] * int(number) + ['.'] * int(gap)
+
+    for i in range(sum([int(x) for x in numbers])):
+        if i >= len(decoded):
+            break
+        if decoded[i] == '.':
+            decoded[i] = pop_rightmost_number(decoded)
+
+    decoded = [num for num in decoded if isinstance(num,int) ]
+
+    return sum([ i * num for i, num in enumerate(decoded)])
+
 
 
 def day09_02(input_data):

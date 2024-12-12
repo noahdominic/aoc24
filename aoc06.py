6a672dc4b300514ac167b72e73d9c9460d0a71ea
noahdominic <noahdmnc@gmail.com>
2024-12-13 02:51:39 +0800

    Update day 06 to fit updated API

--- aoc06.py
+++ aoc06.py
@@ -1,43 +1,35 @@
 UP, RIGHT, DOWN, LEFT = range(4)
 
-def part1(file_path):
-    with open(file_path, 'r') as f:
-        grid = [list(line) for line in f.read().splitlines()]
+def part1(lines):
+    grid = [list(line) for line in lines]
 
-        print(grid)
-        x = 0
-        y = 0
+    x = 0
+    y = 0
 
-        guard_glyphs = ['^', '>', 'v', '<']
+    guard_glyphs = ['^', '>', 'v', '<']
 
-        direction = UP
+    direction = UP
 
-        lx = len(grid[0])
-        ly = len(grid)
-        for i in range(ly):
-            for j in range(lx):
-                if grid[i][j] in guard_glyphs:
-                    x = j
-                    y = i
-                    direction = guard_glyphs.index(grid[i][j])
+    lx = len(grid[0])
+    ly = len(grid)
+    for i in range(ly):
+        for j in range(lx):
+            if grid[i][j] in guard_glyphs:
+                x = j
+                y = i
+                direction = guard_glyphs.index(grid[i][j])
 
-        print(direction, x, y)
+    while True:
+        grid[y][x] = 'X'
+        possible_x, possible_y = next_pos(x, y, direction)
+        if possible_x < 0 or possible_x >= lx or possible_y < 0 or possible_y >= ly:
+            break;
+        if (grid[possible_y][possible_x] == '#'):
+            direction = (direction + 1) % 4
+            continue
+        x, y = (possible_x, possible_y)
 
-        while True:
-            grid[y][x] = 'X'
-            possible_x, possible_y = next_pos(x, y, direction)
-            if possible_x < 0 or possible_x >= lx or possible_y < 0 or possible_y >= ly:
-                break;
-            if (grid[possible_y][possible_x] == '#'):
-                direction = (direction + 1) % 4
-                continue
-            x, y = (possible_x, possible_y)
-
-        for line in grid:
-            print(line)
-
-        print(f"TOTAL: {sum(row.count('X') for row in grid)}")
-
+    return sum(row.count('X') for row in grid)
 
 
 def next_pos(x, y, direction):
